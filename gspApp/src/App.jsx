41f4a5583ebb0e81f1e0eccf68ce9c0db0a90bb8
Ach8393 <achrafsadiq@gmail.com>
2025-01-03 17:45:22 +0100

    first commit

---
+++ gspApp/src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Navbar from './sections/Navbar'
+import Hero from './sections/Hero'
+import About from './sections/About'
+import Projects from './sections/Projects'
+import Clients from './sections/Clients'
+import Experiences from './sections/Experiences'
+import Contact from './sections/Contact'
+import Footer from './sections/Footer'
+
+const App = () => {
+  return (
+    <main className='max-w-7xl mx-auto'>
+      <Navbar />
+      <Hero />
+      <About />
+      <Projects />
+      <Clients />
+      <Experiences />
+      <Contact />
+      <Footer />
+    </main>
+  )
+}
+
+export default App
