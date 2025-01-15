042d89fc9b95e2c3c2d35f82713a6d971ba31419
Ach8393 <achrafsadiq@gmail.com>
2025-01-16 00:57:07 +0100

    return

--- gspApp/vite.config.js
+++ gspApp/vite.config.js
@@ -1,26 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        format: 'es', // Ensure the output format is ES modules
-      },
-    },
-  },
-  server: {
-    headers: {
-      'Content-Type': 'application/javascript', // Ensure correct MIME type for local dev server
-    },
-  },
-  esbuild: {
-    target: 'esnext', // Ensure modern JavaScript features
-  },
-  resolve: {
-    alias: {
-      '@': '/src', // Optional: Create an alias for cleaner imports
-    },
-  },
-});
+})
